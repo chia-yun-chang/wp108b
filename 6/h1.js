529bd3c9cbe1036cb1f2e002d923ff8429f59052
janice <janice881188@gmail.com>
2020-05-05 01:12:33 +0800

    77777

--- 6/h1.js
+++ 6/h1.js
@@ -1,15 +1,14 @@
-dit={"狗":"dog","貓":"cat", "追":"chase","一隻":"a"}; //字典定義
+var dit={"狗":"dog", "貓":"cat", "追":"chase", "一隻":"a", "吃":"eat" };
 function word(a)
 {
-var sum=[]
-for(i=0;i<a.length;i++)
+ var sum=[]
+  for(i=0;i<a.length;i++)
 {
-var eword=a[i]
-var ework=dit[eword]
-//console.log(ework)
-sum.push(ework)
+ var eword=a[i]
+ var ework=dit[eword]
+  sum.push(ework)
 }
-return sum;
+  return sum;
 }
 var a=process.argv.slice(2)
 console.log(word(a));
---
+++ js/json.js
@@ -0,0 +1,14 @@
+var george ={
+    "name": "george" ,
+    "age": 25,
+    "friends": [
+        {"name":"john", "age": 22 },
+        {"name":"mary", "age": 28 }
+    ]
+};
+
+console.log("george.age="+george.age);
+console.log("george.friends:");
+var friends = george.friends;
+for (i in friends)
+ console.log("   "+friends[i].name+" is "+friends[i].age+"years old!");
