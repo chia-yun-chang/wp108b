c7cdd2856c95bed534adf155737c2d2109b6f2a2
janice <janice881188@gmail.com>
2020-05-28 01:53:19 +0800

    1

---
+++ js/random/r.js
@@ -0,0 +1,9 @@
+export function random(a,b){
+    return a+Math.random()*(b-a);
+}
+export function randomInt(a,b){
+    return Math.floor(random(a,b));
+}
+export function sample(array){
+    return array[randomInt(0,array.length)];
+}
