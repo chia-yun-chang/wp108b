d0b24712e14fa786351f512af5f022e00a3c817e
janice <janice881188@gmail.com>
2020-06-02 22:11:25 +0800

    1

--- final/index.js
+++ final/index.js
@@ -21,14 +21,14 @@ window.onclick = function(event) {
   }
   
 function Function1() {
-  document.getElementById("dog").innerHTML = "Dog leash";}
+  document.getElementById("dog").innerHTML = "Dog leash (100*";}
 function Function2() {
-  document.getElementById("trash").innerHTML = "Trash can";}
+  document.getElementById("trash").innerHTML = "Trash can (200*";}
 function Function3() {
-  document.getElementById("toy").innerHTML = "Toy";}
+  document.getElementById("toy").innerHTML = "Toy (300*";}
 function Function4() {
-  document.getElementById("birdcage").innerHTML = "Birdcage";}
+  document.getElementById("birdcage").innerHTML = "Birdcage (400*";}
 function Function5() {
-  document.getElementById("patting").innerHTML = "Patting";}
+  document.getElementById("patting").innerHTML = "Patting (500*";}
 function Function6() {
-  document.getElementById("mug").innerHTML = "Mug";}
+  document.getElementById("mug").innerHTML = "Mug (600*";}
