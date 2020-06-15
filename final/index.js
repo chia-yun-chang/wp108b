a5bb5b03e81d0f0e9c211f0218f611e34606c671
janice <janice881188@gmail.com>
2020-06-15 23:30:59 +0800

    aaa

--- final/index.js
+++ final/index.js
@@ -1,53 +1,21 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";}
- 
+    document.getElementById("mySidenav").style.width = "250px";}
+   
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";}
+document.getElementById("mySidenav").style.width = "0";}
 
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");}
-    
+document.getElementById("myDropdown").classList.toggle("show");}
+
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
-  for (i = 0; i < dropdowns.length; i++) {
+for (i = 0; i < dropdowns.length; i++) {
     var openDropdown = dropdowns[i];
-  if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
-       }
-      }
+if (openDropdown.classList.contains('show')) {
+    openDropdown.classList.remove('show');
+        }
     }
-  }
-  
-function total(id){
-  var quantity=document.getElementById("quantity"+id).value;
-  var price=document.getElementById("price"+id).value;
-  var smallTotal=quantity*price;
-  var smallT=document.getElementById("smallTotal"+id);
-  smallT.innerHTML=smallTotal;
-  var totalPrice=0;
-
-  for(var a=1;a<4;a++){
-    var quantity=document.getElementById("quantity"+a).value;
-    var price=document.getElementById("price"+a).value;
-    var smallTotal=quantity*price;
-    totalPrice=totalPrice+smallTotal;}
-
-  var total=document.getElementById("total");
-  total.innerHTML=totalPrice;}
-
-function initialize(){
-  var totalPrice=0;
-
-  for(var a=1;a<4;a++){
-    var quantity=document.getElementById("quantity"+a).value;
-    var price=document.getElementById("price"+a).value;
-    var smallTotal=quantity*price;
-    totalPrice=totalPrice+smallTotal;
-    var smallT=document.getElementById("smallTotal"+a);
-    smallT.innerHTML=smallTotal;}
-
-  var total=document.getElementById("total");
-  total.innerHTML=totalPrice;}
- 
+    }
+}
